926531653b72c7bf1b5866508c066a393c7cfde2
FabGtte <41702805+FabGtte@users.noreply.github.com>
2020-09-28 16:04:19 +0200

    Add files via upload

---
+++ C# 3. Caracteres.cs
@@ -0,0 +1,13 @@
+using System;
+
+class MainClass {
+  public static void Main (string[] args) {
+    
+    for (int i = 65; i <= 90; i++)
+    {
+      char alphabet = (char)i;
+      Console.WriteLine(alphabet);
+    }
+
+  }
+}
